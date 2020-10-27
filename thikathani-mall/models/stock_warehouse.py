eb2de5d9207d14b51aaaee36033779cdcc8298f0
Kyssnar <ailton.salg@gmail.com>
2020-10-27 10:27:07 -0500

    names

--- thikathani-mall/models/stock_warehouse.py
+++ thikathani-mall/models/stock_warehouse.py
@@ -6,10 +6,6 @@ from odoo import models, fields, api
 
 class Warehouse(models.Model):
     _inherit = 'stock.warehouse'
-    # Supported
-    # 'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
-    #city = fields.One2many('res.partner', 'partner_id', related="partner_id.city", readonly=True)
-
     
     country = fields.Char('Country', related='partner_id.state_id.country_id.name', readonly=True)
     state = fields.Char('State', related='partner_id.state_id.name', readonly=True)
